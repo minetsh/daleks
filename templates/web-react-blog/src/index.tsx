f31b5f06a42acc752c9985ae03d2e679df917a9d
Felix <felix@xingren.com>
2020-05-22 19:41:48 +0800

    feat: 添加 React Blog Project

--- templates/web-react-blog/src/index.tsx
+++ templates/web-react-blog/src/index.tsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
 
+console.log(process.env.NODE_ENV);
+
 ReactDOM.render(<App />, document.getElementById('root'));
