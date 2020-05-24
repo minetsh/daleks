6742d461d18cf6b9e2a52fb49b56f8b0e3bed368
Felix <felix@xingren.com>
2020-05-25 00:00:07 +0800

    feat: 添加 web-react 项目

---
+++ templates/web-react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.scss';
+
+function App() {
+  return <div>Hello React.</div>;
+  // return (
+  //   <Router>
+  //     <Route path="/*" />
+  //   </Router>
+  // );
+}
+
+export default App;
