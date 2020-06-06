acf5dfe27f0d2e664536d7f67dfd90f98f2bd8c1
Felix <felix@xingren.com>
2020-06-06 22:15:35 +0800

    fix: 修复获取列表不统一问题

--- src/commands/list/index.ts
+++ src/commands/list/index.ts
@@ -12,7 +12,7 @@ export interface DaleksConfig {
 
 export const templates = async (): Promise<DaleksConfig> => {
   const response = await Axios.get(
-    `https://api.github.com/repos/minetsh/template/contents/template.json?ref=master`,
+    `https://api.github.com/repos/minetsh/template/contents/daleks.json?ref=master`,
   );
   const { status, data } = response;
   if (status === 200) {
@@ -27,21 +27,11 @@ export const templates = async (): Promise<DaleksConfig> => {
 export default async function (): Promise<void> {
   const o = ora(`获取列表`).start();
   try {
-    const response = await Axios.get(
-      `https://api.github.com/repos/minetsh/template/contents/daleks.json?ref=master`,
-    );
-    const { status, data } = response;
-    if (status === 200) {
-      o.succeed();
-      const { content } = data;
-      const buf = Buffer.from(content, 'base64');
-      const template: DaleksConfig = JSON.parse(buf.toString());
-      (template.templates || []).forEach((t, index: number) => {
-        console.log(`${index + 1}. ${t.name}`);
-      });
-    } else {
-      o.fail(`${response.status}: ${response.statusText}`);
-    }
+    const config = await templates();
+    o.succeed();
+    (config.templates || []).forEach((t, index: number) => {
+      console.log(`${index + 1}. ${t.name}`);
+    });
   } catch (e) {
     o.fail(`${e}`);
   }
