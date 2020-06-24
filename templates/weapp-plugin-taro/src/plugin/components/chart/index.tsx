b7f00ecbc173c650cd7a77fa9e256c5b35b7ec2f
Felix <felix@xingren.com>
2020-06-24 13:49:47 +0800

    feat: 支持 Taro 小程序插件

---
+++ templates/weapp-plugin-taro/src/plugin/components/chart/index.tsx
@@ -0,0 +1,13 @@
+import Taro from "@tarojs/taro";
+import { Component } from "@tarojs/taro";
+import { View } from "@tarojs/components";
+
+type Props = {};
+
+type State = {};
+
+export default class Chart extends Component<Props, State> {
+  render() {
+    return <View>s</View>;
+  }
+}
